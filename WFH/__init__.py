48a6f3f45fa2d52281b188a4363b5ee0fc250e6f
hayleycd <cook.hayley@gmail.com>
2020-08-13 11:20:17 -0700

    Uploading project to github.

---
+++ WFH/__init__.py
@@ -0,0 +1,42 @@
+import logging
+import os
+
+import azure.functions as func
+import ldclient
+import jinja2
+
+#Authorize Launch Darkly
+ldclient.set_sdk_key(os.environ['LDKey'])
+
+#Flags
+flag_keys = [
+    "do-not-disturb",
+    "dog-walked",
+    "dog-fed",
+    "interview",
+    "recording",
+    "dog-walked",
+    "hearing-aids",
+    "run-complete",
+]
+
+#Set up Templating Engine
+loader = jinja2.FileSystemLoader('./WFH')
+env = jinja2.Environment(
+    loader=loader,
+    autoescape=jinja2.select_autoescape(['html', 'xml'])
+)
+template = env.get_template('home.html')
+
+def launch_darkly(flag_keys):
+    flag_status = {}
+    for key in flag_keys:
+        flag_status[key] = ldclient.get().variation(key, {"key": "user@test.com"}, False)
+    return flag_status
+
+
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    logging.info('processed request for home funciton')
+    logging.info('checking LaunchDarkly flags')
+    flag_dict = launch_darkly(flag_keys)
+    return func.HttpResponse(body=template.render(flags = flag_dict), headers={"Content-Type": "text/html; charset=utf-8"})
